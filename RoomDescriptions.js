348ebe736d917ba52def03744004f029c3dfae0d
cemakd <engin.cem.akdemir@gmail.com>
2019-05-18 15:48:16 -0400

    Create RoomDescriptions.js

---
+++ RoomDescriptions.js
@@ -0,0 +1,14 @@
+const firstVisitRoomDescriptions = [
+    ["You go through the opening to find a room full of cobwebs. ", "This room has broken pieces of glass on the floor, you get the uneasy feeling of someone watching you. "],
+    ["You open your eyes to find yourself in the middle of a dimly lit room. ", "No one has been in this room for decades. "],
+]
+
+const subsequentVisitRoomDescriptions = [
+    ["You are back in the room full of cobwebs. ", "You come back to the room with broken glass. "],
+    ["You find yourself back in the room you started your journey. ", "Back in the room with the chest. "]
+]
+
+module.exports = {
+    firstVisitRoomDescriptions: firstVisitRoomDescriptions,
+    subsequentVisitRoomDescriptions: subsequentVisitRoomDescriptions
+}
